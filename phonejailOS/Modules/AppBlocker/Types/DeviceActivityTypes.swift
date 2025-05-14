073d1f692b0fc810ef2ba5719783e8e63b0005c4
CamyH <cameronhunt88@hotmail.com>
2025-05-14 14:16:17 +0100

    Add two custom schedule types

---
+++ phonejailOS/Modules/AppBlocker/Types/DeviceActivityTypes.swift
@@ -0,0 +1,15 @@
+//
+//  DeviceActivityTypes.swift
+//  phonejailOS
+//  Adds custom device activity schedule types to the
+//  DeviceActivityName type
+//
+//  Created by Cameron Hunt on 14/05/2025.
+//
+
+import DeviceActivity
+
+extension DeviceActivityName {
+    static let quickSchedule = DeviceActivityName("quickSchedule")
+    static let customSchedule = DeviceActivityName("customSchedule")
+}
