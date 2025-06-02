6c464c1ec19148ad44acfafec5e1f8ea5a6a857f
CamyH <cameronhunt88@hotmail.com>
2025-06-02 09:14:15 +0100

    Add navigation method to view model

---
+++ phonejailOS/Modules/Navigation/ViewModels/NavigationViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationViewModel.swift
+//  phonejailOS
+//
+//  Created by Cameron Hunt on 30/05/2025.
+//
+
+import Foundation
+
+class NavigationViewModel {
+    @Published var navPath: [DestinationType] = []
+    
+    internal func navigatePage(to: DestinationType) {
+        
+    }
+}
